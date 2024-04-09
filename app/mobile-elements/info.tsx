22a87c4b27b3f81bada08c68698f285f3bfdf44e
Shimadakunn <lcombaret@hotmail.co.jp>
2024-04-09 23:12:13 +0200

    Landing pbw

--- app/mobile-elements/info.tsx
+++ app/mobile-elements/info.tsx
@@ -11,135 +11,137 @@ import Card2 from "@/public/images/6.png";
 import { Button } from "@/components/ui/button";
 
 const Info = () => {
-    const scrollToElement = () => {
-        const targetElement = document.getElementById("targetElement");
-        targetElement?.scrollIntoView({ behavior: "smooth", block: "start" });
-        targetElement?.focus({ preventScroll: true });
-      };
-    return ( 
-        <div className="py-8">
-            <div className="relative p-8 flex items-center justify-center flex-col space-y-4">
-                <h1
-                className="text-lg font-[Monument] text-center"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-once="true"
-                >
-                Detain crypto onchain
-                </h1>
-                <h2
-                className="text-sm text-center text-gray-400"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-delay="100"
-                data-aos-once="true"
-                >
-                Buy crypto 10+ chain with the ease of a few clicks.
-                </h2>
-                <Image
-                    src={Crypto}
-                    height="1000"
-                    width="1000"
-                    className="h-full max-h-80 w-full object-cover rounded-xl"
-                    alt="thumbnail"
-                    draggable={false}
-                />
-                      <Image
-            src={Card}
-            className="absolute bottom-0 left-0 w-[50vw] transform translate-y-[50%] opacity-50"
-            draggable={false}
-            alt="logo"
+  const scrollToElement = () => {
+    const targetElement = document.getElementById("targetElement");
+    targetElement?.scrollIntoView({ behavior: "smooth", block: "start" });
+    targetElement?.focus({ preventScroll: true });
+  };
+  return (
+    <div className="py-8">
+      <div className="relative p-8 flex items-center justify-center flex-col space-y-4">
+        <h1
+          className="text-lg font-[Monument] text-center"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-once="true"
+        >
+          Detain crypto onchain
+        </h1>
+        <h2
+          className="text-sm text-center text-gray-400"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
+          Buy crypto 10+ chain with the ease of a few clicks.
+        </h2>
+        <Image
+          src={Crypto}
+          height="1000"
+          width="1000"
+          className="h-full max-h-80 w-full object-cover rounded-xl"
+          alt="thumbnail"
+          draggable={false}
         />
-            </div>
-            <div className="p-8 flex items-center justify-center flex-col space-y-4">
-                <h1
-                className="text-lg font-[Monument] text-center"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-once="true"
-                >
-                Pay crosschain gas fees
-                </h1>
-                <h2
-                className="text-sm text-center text-gray-400"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-delay="100"
-                data-aos-once="true"
-                >
-                Pay gas fees on any chain in one place. <br/>Choose how much you want to spend.
-                </h2>
-                <Image
-                    src={Gas}
-                    height="1000"
-                    width="1000"
-                    className="h-full max-h-80 w-[80%] object-cover rounded-xl"
-                    alt="thumbnail"
-                    draggable={false}
-                />
-            </div>
-            <div className="relative p-8 flex items-center justify-center flex-col space-y-4">
-                <h1
-                className="text-lg font-[Monument] text-center"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-once="true"
-                >
-                 Access to whole DeFi
-                </h1>
-                <h2
-                className="text-sm text-center text-gray-400"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-delay="100"
-                data-aos-once="true"
-                >
-                 Interact with 20+ protocols on different chains in one place.
-                </h2>
-                <Image
-                    src={Defi}
-                    height="1000"
-                    width="1000"
-                    className="h-full w-full object-cover rounded-xl"
-                    alt="thumbnail"
-                    draggable={false}
-                />
-                <Image
-            src={Card2}
-            className="absolute bottom-0 right-0 w-[50vw] transform translate-y-[50%] opacity-50"
-            draggable={false}
-            alt="logo"
+        <Image
+          src={Card}
+          className="absolute bottom-0 left-0 w-[50vw] transform translate-y-[50%] opacity-50"
+          draggable={false}
+          alt="logo"
         />
-            </div>
-            <div className="p-8 flex items-center justify-center flex-col space-y-4">
-                <h1
-                className="text-lg font-[Monument] text-center"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-once="true"
-                >
-                Your Keys, Your Wallets
-                </h1>
-                <h2
-                className="text-sm text-center text-gray-400"
-                data-aos="fade-up"
-                data-aos-ease="ease-in-out"
-                data-aos-delay="100"
-                data-aos-once="true"
-                >
-                Retreive your wallets whenever you want. You have the full control on your funds, sovereignty is the key.
-                </h2>
-                <Image
-                    src={Wallets}
-                    height="1000"
-                    width="1000"
-                    className="h-full w-[60%] object-cover rounded-xl"
-                    alt="thumbnail"
-                    draggable={false}
-                />
-            </div>
-        </div>
-    );
-}
+      </div>
+      <div className="p-8 flex items-center justify-center flex-col space-y-4">
+        <h1
+          className="text-lg font-[Monument] text-center"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-once="true"
+        >
+          Pay crosschain gas fees
+        </h1>
+        <h2
+          className="text-sm text-center text-gray-400"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
+          Pay gas fees on any chain in one place. <br />
+          Choose how much you want to spend.
+        </h2>
+        <Image
+          src={Gas}
+          height="1000"
+          width="1000"
+          className="h-full max-h-80 w-[80%] object-cover rounded-xl pt-4"
+          alt="thumbnail"
+          draggable={false}
+        />
+      </div>
+      <div className="relative p-8 flex items-center justify-center flex-col space-y-4">
+        <h1
+          className="text-lg font-[Monument] text-center"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-once="true"
+        >
+          Access to whole DeFi
+        </h1>
+        <h2
+          className="text-sm text-center text-gray-400"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
+          Interact with 20+ protocols on different chains in one place.
+        </h2>
+        <Image
+          src={Defi}
+          height="1000"
+          width="1000"
+          className="h-full w-full object-cover rounded-xl"
+          alt="thumbnail"
+          draggable={false}
+        />
+        <Image
+          src={Card2}
+          className="absolute bottom-0 right-0 w-[50vw] transform translate-y-[50%] opacity-50"
+          draggable={false}
+          alt="logo"
+        />
+      </div>
+      <div className="p-8 flex items-center justify-center flex-col space-y-4">
+        <h1
+          className="text-lg font-[Monument] text-center"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-once="true"
+        >
+          Your Keys, Your Wallets
+        </h1>
+        <h2
+          className="text-sm text-center text-gray-400"
+          data-aos="fade-up"
+          data-aos-ease="ease-in-out"
+          data-aos-delay="100"
+          data-aos-once="true"
+        >
+          Retreive your wallets whenever you want. You have the full control on
+          your funds, sovereignty is the key.
+        </h2>
+        <Image
+          src={Wallets}
+          height="1000"
+          width="1000"
+          className="h-full w-[60%] object-cover rounded-xl"
+          alt="thumbnail"
+          draggable={false}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Info;
